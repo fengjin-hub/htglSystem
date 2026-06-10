6a0a823d6aeeabdb20ff768523d9116f07269726
xiaping <hhh@gmail.com>
2026-06-10 21:20:36 +0800

    修复keepAlive导致的动态组件不唯一bug

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -2,9 +2,9 @@
   <div class="app-main">
     <router-view v-slot="{ Component, route }">
       <keep-alive>
-        <component :is="Component" v-if="route.meta.keepAlive" />
+        <component :is="Component" :key="route.name" v-if="route.meta.keepAlive" />
       </keep-alive>
-      <component :is="Component" v-if="!route.meta.keepAlive" />
+      <component :is="Component" :key="route.name" v-if="!route.meta.keepAlive" />
     </router-view>
   </div>
 </template>
